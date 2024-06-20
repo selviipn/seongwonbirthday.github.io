5eb3321c8e88ad2113f9e6e7b8bd24414cb660e6
selviipn <116161026+selviipn@users.noreply.github.com>
2024-06-20 07:38:09 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const title = document.querySelector('.title')
+const text = '오빠 생일 선물로 줄 게 있어'.split('')
+for (let index = 0; index < text.length; index++) {
+  if (text[index] !== ' ') {
+    title.innerHTML += `<span>${text[index]}<span/>`
+  } else {
+    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+  }
+}
+
+const textElements = document.querySelectorAll('.title span');
+textElements.forEach((element) => {
+  const randomDelay = Math.random() * 3; // Menghasilkan delay acak antara 0 hingga 3 detik
+  element.style.animationDelay = `${randomDelay}s`;
+});
